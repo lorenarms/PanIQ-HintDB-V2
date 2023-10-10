7513786d83dce8568ade4f78c2ab3cfc9b966e5a
Lawrence <lorenzo.joseph.dominguez@gmail.com>
2023-10-10 12:25:32 -0700

    added entities

---
+++ API/Entities/Hint.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Entities
+{
+	public class Hint
+	{
+		public int Id { get; set; }
+		public int PuzzleId { get; set; }
+		public int Order { get; set; }
+		public string Description { get; set; }
+
+	}
+}
---
+++ API/Entities/Puzzle.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Entities
+{
+	public class Puzzle
+	{
+		public int Id { get; set; }
+		public int RoomId { get; set; }
+		public int Order { get; set; }
+		public string Name { get; set; }
+
+	}
+}
